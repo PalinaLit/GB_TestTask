6fbdaf41e1bbfb1d2c64fa167540a4fad629f9c3
Palina_Litvinava <palinalitvinava@icloud.com>
2023-02-26 13:24:17 +0300

    add Метод поиска элемента массива определнной длины

--- Task01/Library.cs
+++ Task01/Library.cs
@@ -1,5 +1,9 @@
 public static class Library 
 {
+    /// <summary>
+    /// Метод ввода пользователем массива строк с клавитуры
+    /// </summary>
+    /// <param name="arrayStr">!Заданный! массив строк</param>
     public static void UserInput (string[] arrayStr)
     {
         Console.Write($"Введите элементы массива с клавиатры. Например: пример, вертеброгенная, 1234556, 345, ??ЭЭЖ/");
@@ -22,11 +26,30 @@ public static class Library
     {
         Console.Write($"Ваш массив:  ");
         Console.WriteLine();
-        
+
         for (int i = 0; i < arrayString.Length; i++)
         {
-            Console.Write(arrayString[i]+"   ");
+            Console.Write($"{arrayString[i]} ");
         }
         
+    }
+
+    /// <summary>
+    /// Метод поиска элемента массива определнной длины
+    /// </summary>
+    /// <param name="arrayString">массив для поиска</param>
+    /// <param name="arrayStringNew">массив, для вывода подходящих под критерии элементов</param>
+    /// <param name="elementLength">необходимая длина элемента массива</param>
+    public static void FindStringArrayElementsSpecificLength (string[] arrayString, string[] arrayStringNew, int elementLength)
+    {
+        int count = 0;
+        for (int i = 0; i < arrayString.Length; i++)
+        {
+            if (arrayString[i].Length <= elementLength)
+            {
+                arrayStringNew[count] = arrayString[i];
+                count++;
+            } 
+        }
     }
 }
